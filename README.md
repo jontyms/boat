6cb5bd0c7f535e0bb0e719ae6c683079c0afbb7e
Jonathan Styles <github@jontyms.com>
2024-03-25 20:45:13 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Shitty code for egn boat thing
+
+based on https://howtomechatronics.com/tutorials/arduino/arduino-brushless-motor-control-tutorial-esc-bldc/
+
---
+++ boat-code.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo ESC;     // create servo object to control the ESC
+
+
+void setup() {
+  // Attach the ESC on pin 9
+  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+}
+
+void loop() {
+      sleep(1000);
+      // Arm the ESC
+      ESC.write(1); // Between 0 and 180
+      //write a while loop
+      while(1){
+        ESC.write(30); // Between 0 and 180
+      }
+}
+
